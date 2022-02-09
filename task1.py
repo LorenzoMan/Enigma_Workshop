153fa8adf304d8aecb01e6cd76ab899dbd55c3d3
Lorenzo Manoeuvre <lorenzo.manoeuvre@epitech.eu>
2022-02-09 01:42:50 +0100

    modif

--- task1.py
+++ task1.py
@@ -7,14 +7,7 @@ alphabet ="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 class Data:
     def __init__(self):
-        self.rotor1 = 
-        self.rotor2 = 
-        self.rotor3 = 
-        self.offset1 = 
-        self.offset2 =
-        self.offset3 = 
-        self.encrypt = 
-        self.crypted = 
+        
 
 if __name__=="__main__":
     enigma()
